03865fa0a55fe046fe0293062ec298f95eee429e
jlqykec <jl2005ec@hotmail.com>
2018-02-20 20:19:53 +0800

    minor changes

--- Jacobian_Maple/MARS/quatToRotMatrix.m
+++ Jacobian_Maple/MARS/quatToRotMatrix.m
@@ -1,7 +1,15 @@
-quat1 = [-0.5, 0.5, 0.5, 0.5];
+% Testing quat to rotation matrix to validate Maple transformation matrices
+quat1 = [-0.0, 0.707, 0.707, 0.0];
 quat2 = [0.0, 0.707, 0.707, 0.0];
 quat3 = [-0.053, -0.419, 0.893, 0.155];
 quat4 = [0.108, 0.729, -0.665, -0.123];
 quat5 = [0.059, 0.402, -0.092, 0.909];
 
-rotm = quat2rotm(quat3)
+%ROS order x,y,z,w
+%Matlab order w,x,y,z
+quat = quat1;
+temp = quat(1);
+quat(1:3)=quat(2:4);
+quat(4)=temp;
+
+rotm = quat2rotm(quat)
--- Redundancy_Resolution/MARS_Redundancy_Resolution_Slerp/MARS_UR5/set_MARS_pos_rviz.m
+++ Redundancy_Resolution/MARS_Redundancy_Resolution_Slerp/MARS_UR5/set_MARS_pos_rviz.m
@@ -13,9 +13,15 @@ odom_trans.Header.FrameId = 'odom';
 
 [state_pub, state_msg] = rospublisher('/mars_joint_states','sensor_msgs/JointState');
 
+x=0.0;
+y=0.0;
+phi=-2.0;
+z=0.2;
+ur5=[1.06,-2.60,1.04,1.87,0.35,-0.04];
+
 %Joints values
 state_msg.Name = joints_names;
-state_msg.Position = [0.0; 0.0; 0.0; 0.0; -pi; 3*pi/4; 5*pi/4; -pi/2; 0.0];
+state_msg.Position = [0.0; 0.0; z; ur5(1); ur5(2); ur5(3); ur5(4); ur5(5); ur5(6)];
 state_msg.Velocity = zeros(9,1);
 state_msg.Effort = zeros(9,1);
 
@@ -32,7 +38,7 @@ while (rate.TotalElapsedTime <= 3)
     odom_trans.Transform.Translation.Z = 0.0;
     
     %quatrot = axang2quat([0 0 1 deg2rad(x_pos)]);
-    quatrot = eul2quat([0 0 0],'ZYX');
+    quatrot = eul2quat([phi 0 0],'ZYX');
     odom_trans.Transform.Rotation.W = quatrot(1);
     odom_trans.Transform.Rotation.X = quatrot(2);
     odom_trans.Transform.Rotation.Y = quatrot(3);
