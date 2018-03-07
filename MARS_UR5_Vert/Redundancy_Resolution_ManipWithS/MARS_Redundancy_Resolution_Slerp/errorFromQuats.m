ac8f9dc0c0ecf094a0b7e0370eedd653c03681e4
Josue <jl2005ec.04g@g2.nctu.edu.tw>
2018-03-07 14:57:32 +0800

    tests looking for rotation problem

--- MARS_UR5_Vert/Redundancy_Resolution_ManipWithS/MARS_Redundancy_Resolution_Slerp/errorFromQuats.m
+++ MARS_UR5_Vert/Redundancy_Resolution_ManipWithS/MARS_Redundancy_Resolution_Slerp/errorFromQuats.m
@@ -1,10 +1,10 @@
 function eO=errorFromQuats(qd,qe)
 
 %Create the skew matrix from qd_v
-%S_vd=skewMatrix(qd(2:4));
+S_vd=skewMatrix(qd(2:4));
 
 %Calculate the orientation error
-%eO=qe(1)*qd(2:4)-qd(1)*qe(2:4)-S_vd*qe(2:4);
-eO=qe(1)*qd(2:4)-qd(1)*qe(2:4)-cross(qd(2:4),qe(2:4));
+eO=qe(1)*qd(2:4)-qd(1)*qe(2:4)-S_vd*qe(2:4);
+%eO=qe(1)*qd(2:4)-qd(1)*qe(2:4)-cross(qd(2:4),qe(2:4));
 
 end
