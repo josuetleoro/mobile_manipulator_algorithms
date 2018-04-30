5f62625d268059e63f24af43bb80bfcb00401d88
Josuet Leoro <josuetleoro.eed04g@nctu.edu.tw>
2018-04-30 09:44:41 +0800

    new simulation results

--- MARS_UR5_Vert/Redundancy_Resolution_ManipWithS_Normalized/MM_MotPlan_variable_alpha.m
+++ MARS_UR5_Vert/Redundancy_Resolution_ManipWithS_Normalized/MM_MotPlan_variable_alpha.m
@@ -8,7 +8,7 @@ addpath MARS_UR5
 MARS=MARS_UR5();
 
 %Load the test point
-testN=4;
+testN=7;
 TestPoints
 
 %Set the step size for the gradient descent method and error weight. A
--- MARS_UR5_Vert/Redundancy_Resolution_ManipWithS_Normalized/TestPoints.m
+++ MARS_UR5_Vert/Redundancy_Resolution_ManipWithS_Normalized/TestPoints.m
@@ -112,7 +112,8 @@ switch(testN)
         ty=0;
         phi_mp=0;
         tz=0;
-        qa=[0.0;-pi/2;3*pi/4;5*pi/4;-pi/2;0.0];
+        %qa=[0.0;-pi/2;3*pi/4;5*pi/4;-pi/2;0.0];
+        qa=[0.0;-0.40;1.06;4.06;-pi/2;0.0]; %Joint angles with maximum manipulability
         
         %Desired Pose
         Pos=[1;1;0.6];
