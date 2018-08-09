d1bf296fddaea61f29543fb2d159e9e45c431ac3
Josuet Leoro <josuetleoro.eed04g@nctu.edu.tw>
2018-08-09 17:09:26 +0800

    optimize elbow collision matrix and implement wrist collision
    wrist collision is still not working for test 2

--- MARS_UR5_Vert/Redundancy_Resolution_ManipWithS_Normalized/elbowColMat.m
+++ MARS_UR5_Vert/Redundancy_Resolution_ManipWithS_Normalized/elbowColMat.m
@@ -1,30 +1,28 @@
-function [Wcol, elbowz, dist] = elbowColMat(q,rho,alpha,beta)
+function [Wcol, dist_signed] = elbowColMat(q,rho,alpha,beta)
+%% Returns the Weight matrix for elbow collision
+% Minimum height of the elbow
+elbow_safe_dist = 0.5;
 x=q(1);
 y=q(2);
 phi=q(3);
 zmp=q(4);
 q1=q(5);
 q2=q(6);
-persistent prevGradP;
-if isempty(prevGradP)
-   prevGradP=zeros(9,1) ;
+persistent prevGradPElbow;
+if isempty(prevGradPElbow)
+   prevGradPElbow=zeros(9,1) ;
 end
 
-%% Returns the Weight matrix for elbow collision
-%Calculate elbow position and Jacobian
-%pos_elbow = [-0.425e0 * (-cos(phi) * cos(q1) + sin(phi) * sin(q1)) * cos(q2) - 0.49e-1 * cos(phi) -0.425e0 * (-cos(phi) * sin(q1) - sin(phi) * cos(q1)) * cos(q2) - 0.49e-1 * sin(phi) 0.645359e0 - 0.425e0 * sin(q2) + zmp]';
-pos_elbow = [-0.425e0 * (-cos(phi) * cos(q1) + sin(phi) * sin(q1)) * cos(q2) - 0.49e-1 * cos(phi) + 0.1e1 * x -0.425e0 * (-cos(phi) * sin(q1) - sin(phi) * cos(q1)) * cos(q2) - 0.49e-1 * sin(phi) + 0.1e1 * y 0.645359e0 - 0.425e0 * sin(q2) + zmp]';
-Jelbow = [cos(phi) -0.2125000000e0 * sin(phi + q1 + q2) - 0.2125000000e0 * sin(phi + q1 - q2) + 0.49e-1 * sin(phi) 0 -0.2125000000e0 * sin(phi + q1 + q2) - 0.2125000000e0 * sin(phi + q1 - q2) -0.2125000000e0 * sin(phi + q1 + q2) + 0.2125000000e0 * sin(phi + q1 - q2) 0 0 0 0; sin(phi) 0.2125000000e0 * cos(phi + q1 - q2) + 0.2125000000e0 * cos(phi + q1 + q2) - 0.49e-1 * cos(phi) 0 0.2125000000e0 * cos(phi + q1 - q2) + 0.2125000000e0 * cos(phi + q1 + q2) -0.2125000000e0 * cos(phi + q1 - q2) + 0.2125000000e0 * cos(phi + q1 + q2) 0 0 0 0; 0 0 0.1e1 0 -0.4250000000e0 * cos(q2) 0 0 0 0];
-
-%Jelbow6 = [cos(phi) -0.2125000000e0 * sin(phi + q1 + q2) - 0.2125000000e0 * sin(phi + q1 - q2) + 0.49e-1 * sin(phi) 0 -0.2125000000e0 * sin(phi + q1 + q2) - 0.2125000000e0 * sin(phi + q1 - q2) -0.2125000000e0 * sin(phi + q1 + q2) + 0.2125000000e0 * sin(phi + q1 - q2) 0 0 0 0; sin(phi) 0.2125000000e0 * cos(phi + q1 - q2) + 0.2125000000e0 * cos(phi + q1 + q2) - 0.49e-1 * cos(phi) 0 0.2125000000e0 * cos(phi + q1 - q2) + 0.2125000000e0 * cos(phi + q1 + q2) -0.2125000000e0 * cos(phi + q1 - q2) + 0.2125000000e0 * cos(phi + q1 + q2) 0 0 0 0; 0 0 0.1e1 0 -0.4250000000e0 * cos(q2) 0 0 0 0; 0 0 0 0 -sin(phi + q1) -sin(phi + q1) 0 0 0; 0 0 0 0 cos(phi + q1) cos(phi + q1) 0 0 0; 0 0.1e1 0 0.1e1 0 0 0 0 0;];
-%Jelbow = Jelbow6(1:3,:);
-
-%Vector position of a point on the top of the mobile platform
-mob_plat_height = 0.5;
-pb=[pos_elbow(1);pos_elbow(2);mob_plat_height];
+%% Calculate the elbow position and Jacobian
+% The Jacobian is with respect to a point on the center of the wheels
+% projected to the floor. This frame allows to define the vector pa_pb just
+% as the z position of the elbow minus the safe distance
+pos_elbow_z = 0.645359e0 - 0.425e0 * sin(q2) + zmp;
+Jelbow = [0 0 1 0 -0.4250000000e0 * cos(q2) 0 0 0 0];
 
 %% Calculate the collision gradient
-pa_pb=(pos_elbow-pb);
+pa_pb=pos_elbow_z - elbow_safe_dist;
+dist_signed = pa_pb;
 dist=norm(pa_pb);
 deltad_deltaq=1/dist*(Jelbow'*pa_pb);
 deltaP_deltad=-rho*exp(-alpha*dist)*dist^(-beta)*(beta/dist+alpha);
@@ -34,7 +32,7 @@ gradP = abs(deltaP_deltad*deltad_deltaq);
 % pause()
 %gradP
 Wcol=eye(9,9);
-gradPDif = gradP - prevGradP;
+gradPDif = gradP - prevGradPElbow;
 for i=3:5
     if gradPDif(i) >= 0
         Wcol(i,i) = 1 + gradP(i);
@@ -43,6 +41,6 @@ for i=3:5
     end
 end
 %Wcol(3,3)=1;
-elbowz=pos_elbow(3);
-prevGradP = gradP;
+%elbowz=pos_elbow(3);
+prevGradPElbow = gradP;
 end
