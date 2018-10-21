874385a7afb63be077e9546e818b50f98056782a
Josuet Leoro <jl2005ec@gmail.com>
2018-10-21 21:18:20 +0800

    added original weighting matrix definition but commented

--- MARS_UR5_Vert/MM_Motion_Planning_With_Joints_Constraints/wristColMat.m
+++ MARS_UR5_Vert/MM_Motion_Planning_With_Joints_Constraints/wristColMat.m
@@ -39,6 +39,7 @@ gradP = abs(deltaP_deltad*deltad_deltaq);
 gradPDif = gradP - prevGradPWrist;
 for i=3:9
     if gradPDif(i) >= 0
+        %Wcol(i,i) = 1 + gradP(i);
         Wcol(i,i) = 1/sqrt(1 + gradP(i));
     else
         Wcol(i,i) = 1;
