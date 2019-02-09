bd280144f8faad8b41e79e5da3c8fa4587e9e885
Josuet Leoro <jl2005ec@gmail.com>
2019-02-09 22:19:47 +0800

    update matlab scripts for FK evaluation

--- MARS_UR5_Vert/MM_Motion_Planning_Tests/MARS_UR5/test_sensor_msgs_JointState.m
+++ MARS_UR5_Vert/MM_Motion_Planning_Tests/MARS_UR5/test_sensor_msgs_JointState.m
@@ -1,62 +1,53 @@
 joints_names = {'right_wheel','left_wheel','prismatic_joint','ur5_shoulder_pan_joint','ur5_shoulder_lift_joint'...
     'ur5_elbow_joint','ur5_wrist_1_joint','ur5_wrist_2_joint','ur5_wrist_3_joint'};
 
-rosinit
+%rosinit
 
 %Get the transformation tree
 tftree = rostf;
-pause(1);
 %Create the transformation message
 odom_trans = rosmessage('geometry_msgs/TransformStamped');
 odom_trans.ChildFrameId = 'base_footprint';
 odom_trans.Header.FrameId = 'odom';
 
-%[state_pub, state_msg] = rospublisher('/joint_states','sensor_msgs/JointState');
 [state_pub, state_msg] = rospublisher('/mars_joint_states','sensor_msgs/JointState');
-count = 0;
+
+mob_plat_pos = [0.0;0.0;0.0]; %[x,y,phi]
 prism_pos = 0.0;
-prism_inc = 0.002;
-x_pos = 0.0;
-angle = 0.0;
+%ur5_pos = [0.0;-0.061261057;-1.329242758;2.393719068;-2.646791811;-pi/2;pi];
+ur5_pos = [0.0;0.0;0.0;0.0;0.0;0.0];
+
+%Joints values
+state_msg.Name = joints_names;
+state_msg.Position = [0; 0; prism_pos; ur5_pos];
+state_msg.Velocity = zeros(9,1);
+state_msg.Effort = zeros(9,1);
+
 rate = rosrate(20);
 reset(rate);
-while (rate.TotalElapsedTime <= 10)
+while (rate.TotalElapsedTime <= 5)
     time = rate.TotalElapsedTime;
     fprintf('Time Elapsed: %f\n',time)
-    
-    %Joints values
-    state_msg.Name = joints_names;
-    state_msg.Position = [0.0; 0.0; prism_pos; 0.0; -pi; pi/2; 0.0; 0.0; pi/2];
-    state_msg.Velocity = zeros(9,1);
-    state_msg.Effort = zeros(9,1);
     now = rostime('now');
     
     %Mobile platform movement
-    odom_trans.Transform.Translation.X = 1*cos(angle);
-    odom_trans.Transform.Translation.Y = 1*sin(angle);
+    odom_trans.Transform.Translation.X = mob_plat_pos(1);
+    odom_trans.Transform.Translation.Y = mob_plat_pos(2);
     odom_trans.Transform.Translation.Z = 0.0;
     
-    %quatrot = axang2quat([0 0 1 deg2rad(x_pos)]);
-    quatrot = eul2quat([angle+pi/2 0 0],'ZYZ');
+    quatrot = axang2quat([0 0 1 mob_plat_pos(3)]);
+    %quatrot = eul2quat([pi/2 0 0],'ZYZ');
     odom_trans.Transform.Rotation.W = quatrot(1);
     odom_trans.Transform.Rotation.X = quatrot(2);
     odom_trans.Transform.Rotation.Y = quatrot(3);
     odom_trans.Transform.Rotation.Z = quatrot(4);
-
-    state_msg.Header.Stamp = now;    
+    
+    state_msg.Header.Stamp = now;
     odom_trans.Header.Stamp = now;
     send(state_pub,state_msg);
     sendTransform(tftree, odom_trans);
     
-    %Update joints values
-    x_pos = x_pos + 0.01;    
-    angle = angle + 0.01;    
-    prism_pos = prism_pos + prism_inc;
-    if (prism_pos < 0.0 || prism_pos > 0.4)
-        prism_inc = prism_inc*-1;
-    end
-    
     waitfor(rate);
 end
 
-rosshutdown
+%rosshutdown
--- MARS_UR5_Vert/MM_Motion_Planning_Tests/Simulation Results/set_MARS_pos_rviz.m
+++ MARS_UR5_Vert/MM_Motion_Planning_Tests/Simulation Results/set_MARS_pos_rviz.m
@@ -5,7 +5,6 @@ joints_names = {'right_wheel','left_wheel','prismatic_joint','ur5_shoulder_pan_j
 
 %Get the transformation tree
 tftree = rostf;
-pause(1);
 %Create the transformation message
 odom_trans = rosmessage('geometry_msgs/TransformStamped');
 odom_trans.ChildFrameId = 'base_footprint';
@@ -13,25 +12,30 @@ odom_trans.Header.FrameId = 'odom';
 
 [state_pub, state_msg] = rospublisher('/mars_joint_states','sensor_msgs/JointState');
 
+mob_plat_pos = [0.0;0.0;0.0]; %[x,y,phi]
+prism_pos = 0.0;
+%ur5_pos = [0.0;-0.061261057;-1.329242758;2.393719068;-2.646791811;-pi/2;pi];
+ur5_pos = [0.0;0.0;0.0;0.0;0.0;0.0];
+
 %Joints values
 state_msg.Name = joints_names;
-state_msg.Position = [0.0; 0.0; 0.2; pi/2; -pi/3; 0; pi/2; 0; 0];
+state_msg.Position = [0; 0; prism_pos; ur5_pos];
 state_msg.Velocity = zeros(9,1);
 state_msg.Effort = zeros(9,1);
 
 rate = rosrate(20);
 reset(rate);
-while (rate.TotalElapsedTime <= 10)
+while (rate.TotalElapsedTime <= 5)
     time = rate.TotalElapsedTime;
     fprintf('Time Elapsed: %f\n',time)
     now = rostime('now');
     
     %Mobile platform movement
-    odom_trans.Transform.Translation.X = 0.0;
-    odom_trans.Transform.Translation.Y = 0.0;
+    odom_trans.Transform.Translation.X = mob_plat_pos(1);
+    odom_trans.Transform.Translation.Y = mob_plat_pos(2);
     odom_trans.Transform.Translation.Z = 0.0;
     
-    quatrot = axang2quat([0 0 1 pi/2]);
+    quatrot = axang2quat([0 0 1 mob_plat_pos(3)]);
     %quatrot = eul2quat([pi/2 0 0],'ZYZ');
     odom_trans.Transform.Rotation.W = quatrot(1);
     odom_trans.Transform.Rotation.X = quatrot(2);
