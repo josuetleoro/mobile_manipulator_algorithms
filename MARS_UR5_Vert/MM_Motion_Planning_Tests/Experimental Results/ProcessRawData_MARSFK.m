9fb936b5bf66c65d754dd8837a4ea3d365c4b4cc
Josuet Bernardo <itri454436@itri.org.tw>
2019-04-02 11:11:39 +0800

    mobile platform odometry calculation added

--- MARS_UR5_Vert/MM_Motion_Planning_Tests/Experimental Results/ProcessRawData_MARSFK.m
+++ MARS_UR5_Vert/MM_Motion_Planning_Tests/Experimental Results/ProcessRawData_MARSFK.m
@@ -35,43 +35,66 @@ data_jstates = get_traj_data(data_jstates_raw, stamp_start, traj_duration);
 
 
 %% Joint positions and velocities
-%Check which one is the vector with shortest length
-
+% Check which one is the vector with shortest length
 N_jstates = length(data_jstates);
 N_mob_plat = length(data_mob_plat);
 N_tf = length(data_tf);
-N_all = min([N_jstates, N_mob_plat, N_tf]);
+N_all = min([N_jstates, N_mob_plat, N_tf]) - 1;
 
+% Extract the data correspoding to the shortest length
+time_tf = data_tf(1,1:N_all);
+time_mp_vel = data_mob_plat(1,1:N_all);
 time_jstates = data_jstates(1,1:N_all);
+mp_vel_exp = data_mob_plat(2:3,1:N_all);
 z_pj_exp = data_jstates(2,1:N_all);
 qa_exp = data_jstates(3:8,1:N_all);
 dz_pj_exp = data_jstates(9,1:N_all);
 dqa_exp = data_jstates(10:15,1:N_all);
 
-%% Mobile platform and end effector
-time_tf = data_tf(1,1:N_all);
-ee_xi = zeros(7,N_all);
-mp = zeros(3,N_all);
-mp(1,:)=data_tf(2,1:N_all);
-mp(2,:)=data_tf(3,1:N_all);
+%% Mobile platform and end effector (From vive position and from odom)
 %Create a MMUR5 object
 MARS=MARS_UR5();
+
+% For vive position
+ee_xi = zeros(7,N_all);
+mp = zeros(3,N_all);
+mp(1,:)=data_tf(2,1:N_all); % x
+mp(2,:)=data_tf(3,1:N_all); % y
+
+% For odom
+ee_xi_odom = zeros(7,N_all);
+mp_odom = zeros(3,N_all);
+mp_odom(1,1)=data_tf(2,1); % x
+mp_odom(2,1)=data_tf(3,1); % y
+mp_orient_euler = quat2eul(data_tf(5:8,1)'); %ZYX order
+mp_odom(3,1)=mp_orient_euler(1);
 for i=1:N_all
+    %% Vive pose
     mp_orient_euler = quat2eul(data_tf(5:8,i)'); %ZYX order
     mp(3,i)=mp_orient_euler(1);
     joints=[mp(1,i);mp(2,i);mp(3,i);z_pj_exp(i);qa_exp(:,i)];
     
     %Find the position of the end effector
     Tee=MARS.forwardKin(joints);
-    %pause()
-    %Pos_0=T0(1:3,4)
-    %quat_0=cartToQuat(T0(1:3,1:3))
     ee_xi(1:3,i) = Tee(1:3,4);
     ee_xi(4:7,i) = cartToQuat(Tee(1:3,1:3));
+    
+    %% Odom    
+    if i > 1
+        ts=time_mp_vel(i)-time_mp_vel(i-1);        
+        
+        mp_odom(1,i)=mp_odom(1,i-1)+ts*mp_vel_exp(1,i)*cos(mp_odom(3,i-1));
+        mp_odom(2,i)=mp_odom(2,i-1)+ts*mp_vel_exp(1,i)*sin(mp_odom(3,i-1));
+        mp_odom(3,i)=mp_odom(3,i-1)+ts*mp_vel_exp(2,i);        
+    end    
+    joints=[mp_odom(1,i);mp_odom(2,i);mp_odom(3,i);z_pj_exp(i);qa_exp(:,i)];    
+    %Find the position of the end effector
+    Tee=MARS.forwardKin(joints);
+    ee_xi_odom(1:3,i) = Tee(1:3,4);
+    ee_xi_odom(4:7,i) = cartToQuat(Tee(1:3,1:3));    
 end
-mp_vel_exp = data_mob_plat(2:3,1:N_all);
-time_mp_vel = data_mob_plat(1,1:N_all);
-%ee_xi = data_tf(9:15,:);
+mp = mp_odom;
+ee_xi = ee_xi_odom;
 
 %% Calculate original trajectory with the time_tf vector
 file_path_original_traj = strcat(path, test_name, ".mat");
@@ -82,12 +105,3 @@ PlotEvolutionPretty_Exp
 
 %% Compare original joint velocities with obtained joint velocities
 PlotJointVelComp(strcat(path, test_name, ".mat"),time_mp_vel,mp_vel_exp,time_jstates,dqa_exp)
-
-%% Save data in a .mat file
-%file_name_proc_data = strcat(path, test_name, "_proc.mat");
-%save(file_name_proc_data,'time_tf','mp','ee_xi','time_mp_vel','mp_vel','time_jstates','z_pj','dz_pj','qa','dqa');
-
-
-
-
-
