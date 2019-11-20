d05c8de855b35c36df5d13c6d7490099771c847f
Josuet Leoro <jl2005ec@gmail.com>
2019-11-20 16:44:59 +0800

    normalized manip implemented

--- MARS_UR5_Vert/MM_Motion_Planning_Tests/JacobiansFolder/ManipMax.m
+++ MARS_UR5_Vert/MM_Motion_Planning_Tests/JacobiansFolder/ManipMax.m
@@ -91,11 +91,13 @@ clear all
 % wa_max
 
 %% Mobile manipulator + arm
-alpha = 0.05;
+alpha = 0.01;
 q = rand(10,1);
 q(4) = 0.125;
-%q(5:end)=rand(6,1)*pi/2
-q(5:end)=[0.0;-0.40;1.06;5*pi/4;-pi/2;0.0]
+q(5:end)=rand(6,1)*pi/2;
+%q(5:end)=[0.0;-0.40;1.06;5*pi/4;-pi/2;0.0]
+%q(5:end)=[-0.14533,-0.5708,1.1,-2.0832,-1.5832,0]
+q = [0, 0, 0, 0.125, -0.24533, -0.070796, 0.25, -1.7832, pi/2, 0]';
 wM = 0;
 wM_max = 0;
 while true
@@ -142,23 +144,90 @@ dPa(8,1)=wa*trace(inv_Ja*dJadq8);
 dPa(9,1)=wa*trace(inv_Ja*dJadq9);
 dPa(10,1)=0;
 
-wM = w*wa;
+wM = w+wa;
 if wM > wM_max
     wM_max = wM;
 end
 
-dP = wa*dP + w*dPa;
+dP = dP + dPa;
 q_prev = q;
 q = q + alpha*dP;
 
-error = dot(q-q_prev, q-q_prev);
+error = dot(q-q_prev, q-q_prev)
 if (error <= 1e-35)
     break;
 end
 end
 q
-wM_max
+wM_max    
 
-
-
-    
+% %% Mobile manipulator * arm
+% alpha = 0.01;
+% q = rand(10,1);
+% q(4) = 0.125;
+% %q(5:end)=rand(6,1)*pi/2
+% %q(5:end)=[0.0;-0.40;1.06;5*pi/4;-pi/2;0.0]
+% %q(5:end)=[-0.14533,-0.5708,1.1,-2.0832,-1.5832,0]
+% q = [0, 0, 0, 0.125, -0.12217, -0.5236, 1.0996, -2.1468, -4.7124, 0]';
+% wM = 0;
+% wM_max = 0;
+% while true
+% 
+% %Calculate the Jacobian
+% J=evaluateJBar(q(3),q(5),q(6),q(7),q(8),q(9));
+% [dJdq3,dJdq5,dJdq6,dJdq7,dJdq8,dJdq9]=evaluate_dJbardq(q(3),q(5),q(6),q(7),q(8),q(9));
+% 
+% %Calculate the manipulability measure
+% Jt=J';
+% JJt=J*Jt;
+% w=sqrt(det(JJt));
+% 
+% %Calculate each of the elements of the gradient 
+% inv_JJt=inv(JJt);
+% w_2=w/2; %We use -w because the internal motion is substracted
+% dP(1,1)=0;
+% dP(2,1)=0;
+% dP(3,1)=w_2*trace(inv_JJt*(dJdq3*Jt+J*(dJdq3')));
+% dP(4,1)=0;
+% dP(5,1)=w_2*trace(inv_JJt*(dJdq5*Jt+J*(dJdq5')));
+% dP(6,1)=w_2*trace(inv_JJt*(dJdq6*Jt+J*(dJdq6')));
+% dP(7,1)=w_2*trace(inv_JJt*(dJdq7*Jt+J*(dJdq7')));
+% dP(8,1)=w_2*trace(inv_JJt*(dJdq8*Jt+J*(dJdq8')));
+% dP(9,1)=w_2*trace(inv_JJt*(dJdq9*Jt+J*(dJdq9')));
+% dP(10,1)=0;
+%     
+%     
+% Ja=evaluate_Ja(q(5),q(6),q(7),q(8),q(9));
+% [dJadq5,dJadq6,dJadq7,dJadq8,dJadq9]=evaluate_dJadq(q(5),q(6),q(7),q(8),q(9));
+% 
+% wa=abs(det(Ja));
+% 
+% %Calculate each of the elements of the gradient 
+% inv_Ja=inv(Ja);
+% dPa(1,1)=0;
+% dPa(2,1)=0;
+% dPa(3,1)=0;
+% dPa(4,1)=0;
+% dPa(5,1)=wa*trace(inv_Ja*dJadq5);
+% dPa(6,1)=wa*trace(inv_Ja*dJadq6);
+% dPa(7,1)=wa*trace(inv_Ja*dJadq7);
+% dPa(8,1)=wa*trace(inv_Ja*dJadq8);
+% dPa(9,1)=wa*trace(inv_Ja*dJadq9);
+% dPa(10,1)=0;
+% 
+% wM = w*wa;
+% if wM > wM_max
+%     wM_max = wM;
+% end
+% 
+% dP = wa*dP + w*dPa;
+% q_prev = q;
+% q = q + alpha*dP;
+% 
+% error = dot(q-q_prev, q-q_prev)
+% if (error <= 1e-35)
+%     break;
+% end
+% end
+% q
+% wM_max    
