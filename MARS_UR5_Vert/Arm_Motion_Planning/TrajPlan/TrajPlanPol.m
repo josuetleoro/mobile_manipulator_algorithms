d064f47ccf02d4d1aec722049e1babac3fcfe3e1
Josue Leoro <jl2005ec.04g@g2.nctu.edu.tw>
2020-11-27 17:25:32 +0800

    Update TrajPlanPol plots with latex interpreter

--- MARS_UR5_Vert/Arm_Motion_Planning/TrajPlan/TrajPlanPol.m
+++ MARS_UR5_Vert/Arm_Motion_Planning/TrajPlan/TrajPlanPol.m
@@ -21,9 +21,19 @@ dwf=[0;0;0];
 
 time=0:ts:tf;
 
+%Plots properties
+blue=[0    0.4470    0.7410];
+labelFontSize=14;
+lineWidth=1.8;
+
 set(0,'defaulttextinterpreter','latex')
 set(0,'defaulttextfontname', 'Times')
 set(0,'defaulttextfontsize',16)
+
+set(0, 'defaultAxesTickLabelInterpreter','latex');
+set(0, 'defaultLegendInterpreter','latex');
+set(0,'defaultaxesfontsize',16)
+set(0,'defaultaxesfontname', 'Times')
 
 % %Show the end effector motion in 3D
 % figure()
@@ -37,93 +47,79 @@ set(0,'defaulttextfontsize',16)
 % plotEndEffectorMotion(Rd,5)
 % pause()
 
-% %Plot the evolution of x, y and z
+%Plot the evolution of x, y and z
 % figure()
 % subplot(1,3,1)
-% plot(time,x,'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('x')
-% title('x')
+% plot(time,x,'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$x$')
 % 
 % subplot(1,3,2)
-% plot(time,y,'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('y')
-% title('y')
+% plot(time,y,'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$y$')
 % 
 % subplot(1,3,3)
-% plot(time,z,'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('z')
-% title('z')
+% plot(time,z,'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$z$')
 % 
 % %Plot the evolution of dx, dy and dz
 % figure()
 % subplot(1,3,1)
-% plot(time,dx,'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('dx')
-% title('dx')
+% plot(time,dx,'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$\dot{x}$')
 % 
 % subplot(1,3,2)
-% plot(time,dy,'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('dy')
-% title('dy')
+% plot(time,dy,'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$\dot{d}$')
 % 
 % subplot(1,3,3)
-% plot(time,dz,'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('dz')
-% title('dz')
+% plot(time,dz,'Color',blue','LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$\dot{z}$')
 % 
 % %Plot the evolution of wx, wy and wz
 % figure()
 % subplot(1,3,1)
-% plot(time,w(1,:),'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('wx')
-% title('wx')
+% plot(time,w(1,:),'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$\omega_x$')
 % 
 % subplot(1,3,2)
-% plot(time,w(2,:),'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('wy')
-% title('wy')
+% plot(time,w(2,:),'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$\omega_y$')
 % 
 % subplot(1,3,3)
-% plot(time,w(3,:),'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('wz')
-% title('wz')
-% 
+% plot(time,w(3,:),'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$\omega_z$')
 % 
 % %Plot the evolution of quat
 % time=0:ts:tf;
 % figure()
 % subplot(2,2,1)
-% plot(time,quat(1,:),'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('quat_w')
-% title('quat_w')
+% plot(time,quat(1,:),'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$w_{quat}$')
 % 
 % subplot(2,2,2)
-% plot(time,quat(2,:),'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('quat_x')
-% title('quat_x')
+% plot(time,quat(2,:),'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$x_{quat}$')
 % 
 % subplot(2,2,3)
-% plot(time,quat(3,:),'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('quat_y')
-% title('quat_y')
+% plot(time,quat(3,:),'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$y_{quat}$')
 % 
 % subplot(2,2,4)
-% plot(time,quat(4,:),'r','LineWidth',2); grid on
-% xlabel('time(s)')
-% ylabel('quat_z')
-% title('quat_z')
+% plot(time,quat(4,:),'Color',blue,'LineWidth',2); grid on
+% xlabel('$t(s)$')
+% ylabel('$z_{quat}$')
 % pause()
 
 %Return the motion planning data
