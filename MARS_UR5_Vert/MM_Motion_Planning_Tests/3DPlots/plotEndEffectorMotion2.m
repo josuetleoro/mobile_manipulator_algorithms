0c4c51bb94767026da88f65f5ba4d8b9f2c31ef4
Josuet Leoro <jl2005ec@gmail.com>
2021-03-01 17:44:06 +0800

    Add 3D plot of the trajectory

---
+++ MARS_UR5_Vert/MM_Motion_Planning_Tests/3DPlots/plotEndEffectorMotion2.m
@@ -0,0 +1,41 @@
+function plotEndEffectorMotion2(T6Traj,frame_size)
+
+set(0,'defaulttextinterpreter','latex')
+set(0,'defaulttextfontname', 'Times')
+set(0,'defaulttextfontsize',16)
+
+set(0, 'defaultAxesTickLabelInterpreter','latex');
+set(0, 'defaultLegendInterpreter','latex');
+set(0,'defaultaxesfontsize',16)
+set(0,'defaultaxesfontname', 'Times')
+
+labelFontSize=14;
+
+% plot the position
+pos=reshape(T6Traj(1:3,4,:),3,[]);
+n=length(pos); 
+for i=1:n
+    P1=pos(:,i);
+    %Plot the position of the end effector
+    scatter3(P1(1),P1(2),P1(3),5,'filled','k'); hold on
+end
+
+% plot the orientation
+for i=1:10:n
+    %Plot the position of the end effector
+    plotFrame(T6Traj(:,:,i),1,0.6); hold on
+end
+
+text(pos(1,1)-0.5,pos(2,1),pos(3,1)+0.2,['    ' '$r(0)$'],'HorizontalAlignment','left','interpreter','latex','FontSize',labelFontSize);
+text(pos(1,end),pos(2,end),pos(3,end)+0.1,['    ' '$r(t_f)$'],'HorizontalAlignment','left','interpreter','latex','FontSize',labelFontSize);
+
+axis equal
+% xlim([-1, 2.5])
+% ylim([-1.5, 1.5])
+% zlim([0, 2])
+xlabel('$x(m)$','interpreter','latex','FontSize',labelFontSize);
+ylabel('$y(m)$','interpreter','latex','FontSize',labelFontSize);
+zlabel('$z(m)$','interpreter','latex','FontSize',labelFontSize);
+
+
+end
--- MARS_UR5_Vert/MM_Motion_Planning_Tests/3DPlots/plotFrame.m
+++ MARS_UR5_Vert/MM_Motion_Planning_Tests/3DPlots/plotFrame.m
@@ -1,8 +1,7 @@
-function plotFrame(T,width,name)
+function plotFrame(T,width,scale)
 Pos=T(1:3,4);
 R=T(1:3,1:3);
-quiver3(Pos(1),Pos(2),Pos(3),R(1,1),R(2,1),R(3,1),'r','LineWidth',width,'MaxHeadSize',0.4,'AutoScale','off'); hold on;
-quiver3(Pos(1),Pos(2),Pos(3),R(1,2),R(2,2),R(3,2),'g','LineWidth',width,'MaxHeadSize',0.4,'AutoScale','off');
-quiver3(Pos(1),Pos(2),Pos(3),R(1,3),R(2,3),R(3,3),'b','LineWidth',width,'MaxHeadSize',0.4,'AutoScale','off'); 
-text(Pos(1),Pos(2),Pos(3),['    ' name],'HorizontalAlignment','left','FontSize',10);
+quiver3(Pos(1),Pos(2),Pos(3),R(1,1),R(2,1),R(3,1),scale,'r','LineWidth',width,'MaxHeadSize',0.6); hold on;
+quiver3(Pos(1),Pos(2),Pos(3),R(1,2),R(2,2),R(3,2),scale,'g','LineWidth',width,'MaxHeadSize',0.6);
+quiver3(Pos(1),Pos(2),Pos(3),R(1,3),R(2,3),R(3,3),scale,'b','LineWidth',width,'MaxHeadSize',0.6); 
 end
